9bf09969925d284375406bbbf163fef271a3bc4d
dayhele <dayhelepereira@gmail.com>
2024-06-12 09:32:34 -0300

    ajustando regra pro restante dos outros botões que reproduzem sons

--- main.js
+++ main.js
@@ -2,30 +2,36 @@ const buttonsList = document.querySelector(".teclado");
 const soundsList = document.querySelectorAll("audio");
 
 function setupEvents() {
-  
   buttonsList.addEventListener("click", (buttonElement) => {
     playAudio(buttonElement);
     buttonElement.target.classList.add("ativa");
   });
 
-  buttonsList.addEventListener("keyup", (buttonElement) => {
-    if (buttonElement.key === "Enter") {
-      buttonElement.target.classList.remove("ativa");
+  buttonsList.addEventListener("keydown", (buttonElement) => {
+    if (
+      buttonElement.key === "Enter" ||
+      buttonElement.key === "p" ||
+      buttonElement.key === " "
+    ) {
+      playAudio(buttonElement);
+      buttonElement.target.classList.add("ativa");
     }
   });
 
-}
-
-function playAudio(audioElement) {
-  const audioName = audioElement.target.classList[1].split("_")[1];
-
-  const selectedAudio = [...soundsList].find((sound) => {
-    const soundId = sound.id.split("_")[2];
-    return soundId === audioName;
+  buttonsList.addEventListener("keyup", (buttonElement) => {
+    buttonElement.target.classList.remove("ativa");
+    console.log(buttonElement.target.classList);
   });
 
-  selectedAudio.play();
+  function playAudio(audioElement) {
+    const audioName = audioElement.target.classList[1].split("_")[1];
+
+    const selectedAudio = [...soundsList].find((sound) => {
+      const soundId = sound.id.split("_")[2];
+      return soundId === audioName;
+    });
+
+    selectedAudio.play();
+  }
 }
-
 setupEvents();
-
